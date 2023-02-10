e44169dc7887e541432f12f727bea0f4c8c9b6c5
GC <contact@gcteam.dev>
2023-02-10 07:55:31 -0800

    completed redux store to replace useState hook for counter

---
+++ src/Redux/store.tsx
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from "./CounterSlice";
+
+export const store = configureStore({
+  reducer: {
+    //created counterReducer a new Reducer now we came to store to add that to our reducer object, name we give counter it could be any string name
+    counter: counterReducer,
+  },
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
